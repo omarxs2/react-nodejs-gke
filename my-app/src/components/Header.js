491b1fbd1ae9a84dcdef63fe894658fcbc28cd61
omar_xs2 <omar.soliman306@gmail.com>
2022-03-23 16:34:55 +0200

    new version

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>React With Omar 6666</h1>
+            <h1>React With Omar 1010</h1>
         </div>
     )
 }
