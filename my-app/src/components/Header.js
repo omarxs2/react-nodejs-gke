74a1aa78198eec28f058d5f97a22de0cb76d1d63
omar_xs2 <omar.soliman306@gmail.com>
2022-03-23 16:02:10 +0200

    editing cloud build to auto create image and auto deploy

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>React With Omar 3</h1>
+            <h1>React With Omar 555</h1>
         </div>
     )
 }
