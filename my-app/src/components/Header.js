590cd104e94bce2a373b6390d553ade027d74ad7
omar_xs2 <omar.soliman306@gmail.com>
2022-03-23 13:23:26 +0200

    adding cloud build to automaticly create new docker image on push

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>React With Omar</h1>
+            <h1>React With Omar 2</h1>
         </div>
     )
 }
