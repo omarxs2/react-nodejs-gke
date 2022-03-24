4c661b22898dd0105e0c0c16eae0c11840b13bc3
omar_xs2 <omar.soliman306@gmail.com>
2022-03-25 00:41:03 +0200

    new final push 3

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>React With Omar Suleiman 4</h1>
+            <h1>React With Omar Suleiman 5</h1>
         </div>
     )
 }
